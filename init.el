8711a7aed45be77848414fba2423e904f3102fc7
thisago <thisago@disroot.org>
2025-07-14 09:34:29 -0300

    fix(config): roam2

--- init.el
+++ init.el
@@ -152,7 +152,7 @@
        (nim +lsp)               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +brain +roam +journal +pandoc +pomodoro +present +pretty +roam2)               ; organize your plain life in plain text
+       (org +brain +journal +pandoc +pomodoro +present +pretty +roam2)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
