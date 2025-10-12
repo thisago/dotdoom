9279d890872917476efb4f9531337bb9bd6870c2
thisago <thisago@disroot.org>
2025-10-12 17:38:13 -0300

    feat(config/org): enable async

--- custom.el
+++ custom.el
@@ -3,8 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ignored-local-variable-values '((org-src-preserve-indentation)))
- '(safe-local-variable-values '((eval load-file "create-track.el"))))
+ '(ignored-local-variable-values
+    '((projectile-project-test-cmd . "make test")
+       (projectile-project-compilation-cmd . "make")
+       (org-src-preserve-indentation)))
+ '(package-selected-packages '(kele kubernetes yaml))
+ '(safe-local-variable-values
+    '((gac-automatically-add-new-files-p . t) (eval load-file "create-track.el"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
