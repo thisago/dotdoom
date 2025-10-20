1b72b0f8e81bbce0f4bb1df50136da55e3caff59
thisago <thisago@disroot.org>
2025-10-20 10:57:59 -0300

    chore: custom.el

--- custom.el
+++ custom.el
@@ -4,12 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ignored-local-variable-values
-    '((projectile-project-test-cmd . "make test")
-       (projectile-project-compilation-cmd . "make")
-       (org-src-preserve-indentation)))
+   '((projectile-project-test-cmd . "make test")
+     (projectile-project-compilation-cmd . "make")
+     (org-src-preserve-indentation)))
  '(package-selected-packages '(kele kubernetes yaml))
  '(safe-local-variable-values
-    '((gac-automatically-add-new-files-p . t) (eval load-file "create-track.el"))))
+   '((gac-automatically-add-new-files-p . t) (eval load-file "create-track.el"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
